d072dfd48b1f0aed4519431b20cd4735953d2ee8
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-08-06 12:12:25 -0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,24 +1,55 @@
 #pragma once
 
+#include<deque>
 namespace IceCola {
 
-#include<deque>
-
 	template<class T, class Container = std::deque<T>>
-	class queue
+	class Queue
 	{
 	public:
-		queue(){}
-		bool empty();
-		size_t size();
-		T& front();
-		const T front();
-		T& back();
-		const T& back();
-		void push();
-		void pop();
+		Queue(){}
+
+		bool empty() const
+		{
+			return con.empty();
+		}
+	
+		size_t size() const
+		{
+			return con.size();
+		}
+
+		T& front()
+		{
+			return con.front();
+		}
+
+		const T front() const
+		{
+			return con.front();
+		}
+
+		T& back()
+		{
+			return con.back();
+		}
+
+		const T& back() const
+		{
+			return con.back();
+		}
+		
+		void push(const T& x)
+		{
+			con.push_back(x);
+		}
+
+		void pop()
+		{
+			con.pop_front();
+		}
 	private:
-		deque<T> con;
+		Container con;
 	};
 
 }
